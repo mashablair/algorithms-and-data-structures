71ee8ab0ed7c4262bbc9953921ae6f98fc0baa24
Masha Blair <mashablair@gmail.com>
2019-03-25 09:15:54 -0700

    Solution for chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,46 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+//// my solution
+// function chunk(array, size) {
+//   let newArray = [];
+
+//   while (array.length > 0) {
+//     let removed = array.splice(0, size);
+//     newArray.push(removed);
+
+//     console.log(array);
+//     console.log(newArray);
+//   }
+//   return newArray;
+// }
+
+// function chunk(array, size) {
+//   const chunked = [];
+
+//   for (let element of array) {
+//     const last = chunked[chunked.length - 1];
+
+//     if (!last || last.length === size) {
+//       chunked.push([element]);
+//     } else {
+//       last.push(element);
+//     }
+//   }
+
+//   return chunked;
+// }
+
+function chunk(array, size) {
+  const chunked = [];
+  let i = 0;
+
+  while (i < array.length) {
+    chunked.push(array.slice(i, i + size));
+    i += size;
+  }
+
+  return chunked;
+}
 
 module.exports = chunk;
