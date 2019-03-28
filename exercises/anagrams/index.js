4a88adebf6eca1003d409c0a5acbc847a10feba9
Masha Blair <mashablair@gmail.com>
2019-03-27 17:54:45 -0700

    Much shorter solution to anagrams.

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,53 +8,69 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
+// function anagrams(stringA, stringB) {
+//   // clean up strings by using RegEx
+//   stringA = stringA.replace(/[^\w]/g, "").toLowerCase();
+//   stringB = stringB.replace(/[^\w]/g, "").toLowerCase();
+
+//   console.log(stringA);
+//   console.log(stringB);
+
+//   if (stringA.length !== stringB.length) {
+//     return false;
+//   }
+
+//   // build 2 char maps
+//   const obj1 = {};
+//   const obj2 = {};
+
+//   // loop through string A and stringB
+//   function buildCharMap(str, obj) {
+//     for (let char of str) {
+//       if (char in obj) {
+//         obj[char]++;
+//       } else {
+//         obj[char] = 1;
+//       }
+//     }
+//   }
+
+//   buildCharMap(stringA, obj1);
+//   buildCharMap(stringB, obj2);
+
+//   console.log(obj1);
+//   console.log(obj2);
+
+//   // compare obj1 and obj2
+//   if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+//     return false;
+//   }
+
+//   // compare charMaps!
+//   for (let char in obj1) {
+//     if (obj1[char] !== obj2[char]) {
+//       return false;
+//     }
+//   }
+
+//   // don't forget a return statement
+//   return true;
+// }
+
+// easier solution but worse performance
+// it uses array.sort()
 function anagrams(stringA, stringB) {
-  // convert original strings by using RegEx
-  stringA = stringA.replace(/[^\w]/g, "").toLowerCase();
-  stringB = stringB.replace(/[^\w]/g, "").toLowerCase();
+  return _cleanString(stringA) === _cleanString(stringB);
+}
 
-  console.log(stringA);
-  console.log(stringB);
-
-  if (stringA.length !== stringB.length) {
-    return false;
-  }
-
-  // build 2 char maps
-  const obj1 = {};
-  const obj2 = {};
-
-  // loop through string A and stringB
-  function buildCharMap(str, obj) {
-    for (let char of str) {
-      if (char in obj) {
-        obj[char]++;
-      } else {
-        obj[char] = 1;
-      }
-    }
-  }
-
-  buildCharMap(stringA, obj1);
-  buildCharMap(stringB, obj2);
-
-  console.log(obj1);
-  console.log(obj2);
-
-  // compare obj1 and obj2
-  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
-    return false;
-  }
-
-  // compare charMaps!
-  for (let char in obj1) {
-    if (obj1[char] !== obj2[char]) {
-      return false;
-    }
-  }
-
-  // don't forget a return statement
-  return true;
+// helper func to clean up strings
+function _cleanString(str) {
+  return str
+    .replace(/[^\w]/g, "")
+    .toLowerCase()
+    .split("")
+    .sort()
+    .join("");
 }
 
 module.exports = anagrams;
