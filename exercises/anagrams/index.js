7f3d869c3159e83dc509e4ba544925b2fb599c0f
Masha Blair <mashablair@gmail.com>
2019-03-27 17:38:45 -0700

    My solution to anagrams.

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,53 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  // convert original strings by using RegEx
+  stringA = stringA.replace(/[^\w]/g, "").toLowerCase();
+  stringB = stringB.replace(/[^\w]/g, "").toLowerCase();
+
+  console.log(stringA);
+  console.log(stringB);
+
+  if (stringA.length !== stringB.length) {
+    return false;
+  }
+
+  // build 2 char maps
+  const obj1 = {};
+  const obj2 = {};
+
+  // loop through string A and stringB
+  function buildCharMap(str, obj) {
+    for (let char of str) {
+      if (char in obj) {
+        obj[char]++;
+      } else {
+        obj[char] = 1;
+      }
+    }
+  }
+
+  buildCharMap(stringA, obj1);
+  buildCharMap(stringB, obj2);
+
+  console.log(obj1);
+  console.log(obj2);
+
+  // compare obj1 and obj2
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+    return false;
+  }
+
+  // compare charMaps!
+  for (let char in obj1) {
+    if (obj1[char] !== obj2[char]) {
+      return false;
+    }
+  }
+
+  // don't forget a return statement
+  return true;
+}
 
 module.exports = anagrams;
